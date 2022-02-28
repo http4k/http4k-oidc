3b6265c86ccab7e5e824813e53f9ab8ecc8e590b
Ivan Sanchez <ivan@gourame.com>
2022-02-28 22:32:59 +0000

    Remove unused code

--- src/main/kotlin/org/http4k/formats/JacksonMessage.kt
+++
@@ -1,8 +0,0 @@
-package org.http4k.formats
-
-import org.http4k.core.Body
-import org.http4k.format.Jackson.auto
-
-data class JacksonMessage(val subject: String, val message: String)
-
-val jacksonMessageLens = Body.auto<JacksonMessage>().toLens()

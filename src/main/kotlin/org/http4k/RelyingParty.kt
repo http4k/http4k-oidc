7e222e70f5eff9d6bc4a5558287f7a3d11ba280f
Ivan Sanchez <ivan@gourame.com>
2022-02-28 22:49:42 +0000

    Split main file

---
+++ src/main/kotlin/org/http4k/RelyingParty.kt
@@ -0,0 +1,29 @@
+package org.http4k
+
+import org.http4k.core.*
+import org.http4k.core.Method.GET
+import org.http4k.filter.ClientFilters
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.security.InsecureCookieBasedOAuthPersistence
+import org.http4k.security.OAuthProvider
+
+fun RelyingParty(
+    oauthProvider: OAuthProvider,
+    oAuthPersistence: InsecureCookieBasedOAuthPersistence,
+    client: HttpHandler
+) = routes(
+    "/health" bind GET to { Response(Status.OK) },
+    "/oauth" bind routes(
+        "/" bind GET to oauthProvider.authFilter.then {
+            val token = oAuthPersistence.retrieveToken(it)?.value ?: error("not authenticated")
+            val authenticatedClient = ClientFilters.BearerAuth(token).then(client)
+            val userInfoEndpoint = oauthProvider.providerConfig.apiBase.extend(Uri.of("/test/a/http4k-oidc/userinfo"))
+
+            val userInfo = authenticatedClient(Request(GET, userInfoEndpoint)).bodyString()
+
+            Response(Status.OK).body(userInfo)
+        },
+        "/callback" bind GET to oauthProvider.callback
+    )
+)
--- src/main/kotlin/org/http4k/Server.kt
+++ src/main/kotlin/org/http4k/Server.kt
@@ -7,47 +7,20 @@ import org.http4k.client.JavaHttpClient
 import org.http4k.cloudnative.env.Environment
 import org.http4k.cloudnative.env.EnvironmentKey
 import org.http4k.cloudnative.env.Port
-import org.http4k.core.*
-import org.http4k.core.Method.GET
-import org.http4k.core.Status.Companion.OK
-import org.http4k.filter.ClientFilters
+import org.http4k.core.Credentials
+import org.http4k.core.Uri
+import org.http4k.core.extend
+import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters
 import org.http4k.filter.inIntelliJOnly
 import org.http4k.lens.port
 import org.http4k.lens.secret
 import org.http4k.lens.uri
-import org.http4k.routing.bind
-import org.http4k.routing.routes
 import org.http4k.security.InsecureCookieBasedOAuthPersistence
 import org.http4k.security.OAuthProvider
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
-
-fun app(
-    oauthProvider: OAuthProvider,
-    oAuthPersistence: InsecureCookieBasedOAuthPersistence,
-    client: HttpHandler
-) = routes(
-    "/health" bind GET to { Response(OK) },
-    "/oauth" bind routes(
-        "/" bind GET to oauthProvider.authFilter.then {
-            val authenticatedClient =
-                ClientFilters.BearerAuth(oAuthPersistence.retrieveToken(it)?.value ?: error("not authenticated"))
-                    .then(client)
-
-            val userInfo = authenticatedClient(
-                Request(
-                    GET,
-                    oauthProvider.providerConfig.apiBase.extend(Uri.of("/test/a/http4k-oidc/userinfo"))
-                )
-            ).bodyString()
-
-            Response(OK).body(userInfo)
-        },
-        "/callback" bind GET to oauthProvider.callback
-    )
-)
 
 object Config {
     val port = EnvironmentKey.port().defaulted("PORT", Port(9000))
@@ -72,7 +45,7 @@ fun main() {
         oAuthPersistence
     )
 
-    val printingApp = stack.then(app(oauthProvider, oAuthPersistence, client))
+    val printingApp = stack.then(RelyingParty(oauthProvider, oAuthPersistence, client))
     val server = printingApp.asServer(Undertow(port(environment).value)).start()
 
     println("Server started on " + server.port())

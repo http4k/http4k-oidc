ba3b011855f01ae0eb2d9ba45cc9566885b90d08
Ivan Sanchez <ivan@gourame.com>
2022-03-15 20:59:17 +0000

    Use http4k's BasicAuthAccessTokenFetcherAuthenticator

--- relying-party/src/main/kotlin/org/http4k/ProviderDefinition.kt
+++ relying-party/src/main/kotlin/org/http4k/ProviderDefinition.kt
@@ -2,7 +2,6 @@ package org.http4k
 
 import org.http4k.core.Credentials
 import org.http4k.core.HttpHandler
-import org.http4k.core.Request
 import org.http4k.core.Uri
 import org.http4k.security.*
 
@@ -29,14 +28,5 @@ fun OAuthProvider.Companion.oidcAuthServer(
         CrossSiteRequestForgeryToken.SECURE_CSRF,
         accessTokenFetcherAuthenticator = BasicAuthAccessTokenFetcherAuthenticator(providerConfig),
     )
-}
-
-
-class BasicAuthAccessTokenFetcherAuthenticator(private val providerConfig: OAuthProviderConfig) :
-    AccessTokenFetcherAuthenticator {
-    override fun authenticate(request: Request) =
-        request.header("Authorization", "Basic ${providerConfig.credentials.base64Encoded()}")
-
-    private fun Credentials.base64Encoded(): String = "$user:$password".base64Encode()
 }
 

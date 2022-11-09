e3394d572640164e75e3a9cf6a82064042884a94
Ivan Sanchez <29394+s4nchez@users.noreply.github.com>
2022-11-09 08:36:50 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("de.fayard.refreshVersions") version "0.50.2"
-////                            # available:"0.51.0"
+    id("de.fayard.refreshVersions") version "0.51.0"
 }
 
 rootProject.name = "http4k-oidc"

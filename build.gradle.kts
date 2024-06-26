0d9cb9dfa2adb9ec52eb2f19f6e8844201592b33
Ivan Sanchez <ivan@gourame.com>
2024-06-26 10:59:15 +0100

    Upgrade Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ subprojects {
     tasks {
         withType<KotlinCompile> {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "11"
             }
         }
 
